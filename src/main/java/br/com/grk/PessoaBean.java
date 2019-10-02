95c1f4aac379d4524fba2ed238238dbdf8b7d6de
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2019-10-02 17:32:25 -0300

    @SessionScoped: o estado do ManagedBean é mantido até o fim da sessão do usuário

--- src/main/java/br/com/grk/PessoaBean.java
+++ src/main/java/br/com/grk/PessoaBean.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 //@RequestScoped : todos os objetos armazenados no escopo request, sobrevivem apenas a uma submissão ao ciclo de vida do JSF 
 //@ViewScoped :  mantém o estado do bean enquanto houver requisições da mesma view/página, e quando ele muda de página o estado do bean é descartado
+//@SessionScoped: o estado do ManagedBean é mantido até o fim da sessão do usuário.
 
 @ManagedBean(name = "pessoaBean")
-@ViewScoped
+@SessionScoped
 public class PessoaBean {
 	
 	private String nome;
