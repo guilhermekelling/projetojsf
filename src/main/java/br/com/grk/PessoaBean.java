eceb99f136004762be41085c7de245c0b9bb9a1f
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2019-10-03 08:31:52 -0300

    Exemplo de binding - Backing bean componentes html jsf

--- src/main/java/br/com/grk/PessoaBean.java
+++ src/main/java/br/com/grk/PessoaBean.java
@@ -3,8 +3,9 @@ package br.com.grk;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
 //@RequestScoped : todos os objetos armazenados no escopo request, sobrevivem apenas a uma submissão ao ciclo de vida do JSF 
 //@ViewScoped :  mantém o estado do bean enquanto houver requisições da mesma view/página, e quando ele muda de página o estado do bean é descartado
@@ -12,16 +13,31 @@ import javax.faces.bean.ManagedBean;
 //@ApplicationScoped: os dados serão compartilhados por todos os usuários
 
 @ManagedBean(name = "pessoaBean")
-@ApplicationScoped
+@ViewScoped
 public class PessoaBean {
 	
 	private String nome;
+	
+	private HtmlCommandButton commandButton;
 			
 	private List<String> nomes = new ArrayList<String>();
 	
 	public String addNome() {
 		nomes.add(nome);
+		
+		if (nomes.size() > 3) {
+			commandButton.setDisabled(true);
+		}
+		
 		return "";
+	}
+	
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+	
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
 	}
 	
 	public void setNomes(List<String> nomes) {
